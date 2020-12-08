44a9d244dcb65799fbddbcc8f37e8cd5807181e6
Anatole Lamarque <lamarqueanatole@gmail.com>
2020-12-08 15:35:37 +0100

    adds plotting

--- solver.py
+++ solver.py
@@ -1,73 +1,26 @@
 import csv
 import sys
-import math
-
-# Heuristics
-def norm(A, B):
-    return math.sqrt(pow(B.x - A.x, 2) + pow(B.y - A.y, 2))
-
-def squaredNorm(A, B):
-    return pow(B.x - A.x, 2) + pow(B.y - A.y, 2)
-
-def manhattan(A, B):
-    return B.x - A.x + B.y - A.y
-
-# Describes a position on the plan
-class Position:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __str__(self):
-        return "(%d, %d)" % (self.x, self.y)
-
-# A point in the VRP problem, with coordinates, wight and utility
-class Point:
-    def __init__(self, index, x, y, weight, utility):
-        self.index = index
-        self.pos = Position(x, y)
-        self.weight = weight
-        self.utility = utility
-
-    def value(self):
-        if self.weight != 0:
-            return self.utility/self.weight
-        else:
-            return self.utility
-
-    def __str__(self):
-        return "i: %d, pos: %s, weight: %d, utility: %d (, value: %.2f)" % (self.index, self.pos, self.weight, self.utility, self.value())
-
-# A vehicle in the VRP problem with a capacity
-class Vehicle:
-    def __init__(self, index, capacity, warehouse):
-        self.index = index
-        self.capacity = capacity
-        self.route = list()
-        self.totalDistance = 0
-        self.warehouse = warehouse
-
-    def addPointToRoute(self, point):
-        if len(self.route) == 0:
-            self.totalDistance = norm(self.warehouse, point.pos)
-        else:
-            self.totalDistance += norm(self.route[-1].pos, point.pos)
-
-        self.route.append(point)
-
-    def __str__(self):
-        return "i: %d, capacity: %d" % (self.index, self.capacity)
+import matplotlib.pyplot as plt
+from matplotlib.path import Path
+import matplotlib.patches as patches
+from enum import Enum
+from utils import distanceType, Point, Position, Vehicle, norm, manhattan, squaredNorm
 
 # The solver itself
 class Solver:
-    def __init__(self):
+    def __init__(self, path, method):
         print('Solver started. Please load an instance')
-        self.i = None
-        self.nbVehicles = None
-        self.nbPoints = None
         self.vehicles = list()
         self.points = list()
-        self.warehouse = None
+        self.method = method
+        self.load(path)
+
+        if (method == distanceType.MANHATTAN):
+            self.computeDistance = manhattan
+        elif (method == distanceType.NORM):
+            self.computeDistance = norm
+        elif (method == distanceType.SQUARED_NORM):
+            self.computeDistance = squaredNorm
 
     def load(self, path):
         print('Loading \'' + path + '\'')
@@ -79,18 +32,18 @@ class Solver:
             self.nbPoints = int(rows[2][0])
             self.warehouse = Position(int(rows[3][0]), int(rows[3][1]))
             for i in range(4, 4 + self.nbVehicles):
-                self.vehicles.append(Vehicle(int(rows[i][0]), int(rows[i][1]), self.warehouse))
+                self.vehicles.append(Vehicle(float(rows[i][0]), float(rows[i][1]), self.warehouse))
             for i in range(4 + self.nbVehicles, 4 + self.nbVehicles + self.nbPoints):
-                self.points.append(Point(int(rows[i][0]), int(rows[i][1]), int(rows[i][2]), int(rows[i][3]), int(rows[i][4])))
+                self.points.append(Point(float(rows[i][0]), float(rows[i][1]), float(rows[i][2]), float(rows[i][3]), float(rows[i][4])))
 
-    def computeRoadForVehicle(self, vehicle, computeDistance):
+    def computeRoadForVehicleClosest(self, vehicle):
         def closestUnvisitedPoint(unvisitedPoints):
             distanceToChosenPoint = sys.float_info.max
             for currentUnvisitedPoint in unvisitedPoints:
                 if len(vehicle.route) == 0:
-                    distanceToCurrentPoint = computeDistance(self.warehouse, currentUnvisitedPoint.pos)
+                    distanceToCurrentPoint = self.computeDistance(self.warehouse, currentUnvisitedPoint.pos)
                 else:
-                    distanceToCurrentPoint = computeDistance(vehicle.route[-1].pos, currentUnvisitedPoint.pos)
+                    distanceToCurrentPoint = self.computeDistance(vehicle.route[-1].pos, currentUnvisitedPoint.pos)
 
                 if distanceToCurrentPoint < distanceToChosenPoint:
                     distanceToChosenPoint = distanceToCurrentPoint
@@ -105,9 +58,14 @@ class Solver:
             chosenPoint = closestUnvisitedPoint(unvisitedPoints)
             vehicle.addPointToRoute(chosenPoint)
             unvisitedPoints.remove(chosenPoint)
+    
+    # def computeRoadForVehicleInsertion(self, vehicle):
+    #     unvisitedPoints = [point for point in self.points]
+    #     while len(unvisitedPoints):
+    #         if len(self.vehicles)
 
     def solve(self):
-        self.computeRoadForVehicle(self.vehicles[0], squaredNorm)
+        self.computeRoadForVehicleClosest(self.vehicles[0])
 
     def dumpInstance(self):
         print("I: %s" % self.i)
@@ -125,3 +83,21 @@ class Solver:
             print("Distance: %d" % vehicle.totalDistance)
             for point in vehicle.route:
                 print(point)
+
+    def plotRoads(self, figpath):
+        verts = [(self.warehouse.x, self.warehouse.y)] + self.vehicles[0].routeToVertices() + [(self.warehouse.x, self.warehouse.y)]
+
+        codes = [Path.MOVETO]
+        for i in range(len(verts) - 2):
+            codes.append(Path.LINETO)
+        codes.append(Path.CLOSEPOLY)
+
+        path = Path(verts, codes)
+
+        fig, ax = plt.subplots()
+        patch = patches.PathPatch(path, facecolor='none', lw=2)
+        ax.add_patch(patch)
+        ax.set_xlim(-1000, 1000)
+        ax.set_ylim(-1000, 1000)
+        fig.suptitle("Total distance: %d, computed with %s" % (self.vehicles[0].totalDistance, self.method.value))
+        plt.savefig(figpath)
